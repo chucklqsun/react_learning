c35c15e2af818ddaac55ee7a35d9502c03adf46e
chucklqsun <bartowski.sun@gmail.com>
2019-07-14 14:48:19 +1000

    ignore .idea add ajax

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
 import './App.css';
-// import Toggle from '../Toggle';
+import Toggle from '../Toggle';
 import FilterableProductTable from "../FilterableProductTable";
 
-const PRODUCTS = [
-    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <FilterableProductTable products={PRODUCTS}/>
-      </header>
-    </div>
-  );
+// const PRODUCTS = [
+//     {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+//     {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+//     {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+//     {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+//     {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+//     {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+// ];
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3001/product_list")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <FilterableProductTable products={this.state.items}/>
+                <Toggle/>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { styled } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
 
 class Toggle extends React.Component {
@@ -17,10 +18,21 @@ class Toggle extends React.Component {
     }
 
     render() {
+        const MyButton = styled(Button)({
+            background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+            border: 0,
+            borderRadius: 3,
+            boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+            color: 'white',
+            height: 48,
+            padding: '0 30px',
+            margin: '5px 0',
+            minWidth: '110px'
+        });
         return (
-            <Button onClick={this.handleClick} color="primary">
+            <MyButton onClick={this.handleClick} color="primary">
                 {this.state.isToggleOn ? 'ON' : 'OFF'}
-            </Button>
+            </MyButton>
         );
     }
 }
